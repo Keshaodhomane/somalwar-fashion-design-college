93d9fd029ed998b2479bab902ab16a2aba143d97
Keshao Dhomane <keshaodhomane@gmail.com>
2020-08-29 11:58:25 +0530

    js updates

--- js/main.js
+++ js/main.js
@@ -18,7 +18,37 @@ $('.eventbanner').slick({
   autoplaySpeed: 6000,
   speed: 800,
   slidesToShow: 2,
-  adaptiveHeight: true
+    
+  adaptiveHeight: true ,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  
 });
 
 
@@ -30,7 +60,36 @@ $('.logos').slick({
   autoplaySpeed: 6000,
   speed: 800,
   slidesToShow: 4,
-  adaptiveHeight: true
+  
+  adaptiveHeight: true ,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
